f551d49b2256eb2ea8f799a3886868881908dd1e
Nat <nathalie_sa@live.fr>
2024-03-22 13:05:49 +0100

    test affichage page

--- pages/_app.js
+++ pages/_app.js
@@ -38,6 +38,7 @@ function App({ Component, pageProps }) {
       <PersistGate persistor={persistor}>
         <Head>
           <title>Flowst</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <Component {...pageProps} />
       </PersistGate>
